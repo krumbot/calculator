33f1b91da41057cf34c1ac15c45d33ef56258f3a
Vikrum Joshi <vikrum@Vikrums-MacBook-Pro.local>
2016-12-31 15:11:09 -0500

    input added in and buttonsection initialized

---
+++ src/components/ButtonSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ButtonSection extends Component {
+	render() {
+		return (
+			<View> 
+				<Text> Testing ButtonSection </Text>
+			</View>
+	);
+	}
+}
+
+export default ButtonSection;
---
+++ src/components/InputSection.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { TextInput, View } from 'react-native';
+
+class InputSection extends Component {
+	render() {
+		const { containerStyle, inputStyle } = styles;
+		return (
+			<View style={containerStyle}>
+				<TextInput 
+					style={inputStyle}
+					placeholder="123456"
+					onChangeText={() => console.log('changed!')}
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = {
+	inputStyle: {
+		color: '#000',
+		paddingRight: 5,
+		paddingLeft: 5,
+		fontSize: 18,
+		lineHeight: 23,
+		flex: 1
+	},
+	containerStyle: {
+		height: 40,
+		flex: 1,
+		flexDirection: 'row'
+	}
+};
+
+export default InputSection;
