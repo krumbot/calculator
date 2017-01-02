214fdd1dff6868fe2112a60086cb5f77b0d73b53
Vikrum Joshi <vikrum@Vikrums-MBP.home>
2017-01-02 15:25:55 -0500

    reset opreation added

--- src/reducers/CalcReducer.js
+++ src/reducers/CalcReducer.js
@@ -9,35 +9,48 @@ const initialState = {
 	displayValue: '',
 	trueValue: '',
 	calculatedValue: '',
-	operator: ''
+	operator: '',
+	reset: false
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case NUMERIC_PRESS:
+			if (state.reset) {
+				return {
+					...state,
+					displayValue: action.payload,
+					trueValue: action.payload,
+					calculatedValue: eval(action.payload),
+					operator: '',
+					reset: false
+				};
+			}
 			return { 
 				...state, 
 				displayValue: state.displayValue + action.payload,
 				trueValue: state.trueValue + action.payload,
 				calculatedValue: eval(state.trueValue + action.payload),
-				operator: ''
+				operator: '',
+				reset: false
 			};
 		case CLEAR_PRESS:
 			return { ...initialState };
 		case EQUALS_PRESS:
-			console.log('equals press');
 			return { 
 				...state,
 				displayValue: state.calculatedValue.toString(),
 				trueValue: state.calculatedValue.toString(),
-				operator: ''
+				operator: '',
+				reset: true
 			};
 		case OPERATOR_PRESS:
 			return {
 				...state,
 				displayValue: state.displayValue + action.payload.display,
 				trueValue: state.trueValue + action.payload.value,
-				operator: action.payload
+				operator: action.payload,
+				reset: false
 			};
 		default:
 			return state;
