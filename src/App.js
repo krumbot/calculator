c7810a7ffc6f7569542d0c700a74372f2996b92f
Vikrum Joshi <vikrum@Vikrums-MBP.home>
2017-05-30 17:45:51 -0400

    removing comments

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import { createStore } from 'redux';
 import RouterComponent from './Router';
 import reducers from './reducers';
 
-// import ReduxThunk from 'redux-thunk'; 
-// import firebase from 'firebase';
-// import reducers from './reducers';
-// import Router from './Router';
-
 class App extends Component {
 	render() {
 		const store = createStore(reducers);
